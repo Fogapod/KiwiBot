3e35f89a659a2ffdf23def158ed06f1532111cd1
Fogapod <gish.ee18@gmail.com>
2018-02-15 23:54:27 +0300

    Added ddg module

---
+++ modules/module_ddg.py
@@ -0,0 +1,50 @@
+from modules.modulebase import ModuleBase
+from utils.formatters import format_response
+
+from discord import File
+
+from aiohttp import ClientSession
+
+
+API_URL = 'https://api.duckduckgo.com'
+
+class Module(ModuleBase):
+    """{prefix}{keywords} <text>
+
+    Web search using duckduckgo.
+    {protection} or higher permission level required to use"""
+
+    name = 'ddg'
+    keywords = (name, 'duckduckgo')
+    arguments_required = 1
+    protection = 0
+
+    async def on_call(self, msg, *args):
+        params = {
+            'q': ' '.join(args[1:]),
+            'o': 'json'
+        }
+
+        async with ClientSession() as s:
+            async with s.get(API_URL, params=params) as r:
+                if r.status != 200:
+                    return '{error} request failed: ' + str(r.status)
+                r_json = await r.json(content_type='application/x-javascript')
+            
+            result = r_json['AbstractText']
+
+            if not result:
+                return 'Nothing found'
+            else:
+                image_url = r_json['Image']
+
+            image = None
+            if image_url:
+                async with s.get(image_url) as r:
+                    if r.status == 200:
+                        image = File(await r.content.read(), filename='file.png')
+                    else:
+                        result += '{error} failed to fetch image: ' + image_url
+
+        result = await format_response(result, msg, self.bot)
+        await self.bot.send_message(msg, result, file=image)                     
