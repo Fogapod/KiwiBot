8e789dcb7d04fc128380833245122bbde6d46dde
Fogapod <gish.ee18@gmail.com>
2018-03-05 09:26:40 +0300

    added info module, using embed in invite module

---
+++ modules/module_info.py
@@ -0,0 +1,71 @@
+from modules.modulebase import ModuleBase
+
+from utils.helpers import (
+    create_subprocess_shell, execute_process, get_local_prefix)
+from utils.constants import AUTHOR_ID
+
+from discord import Colour, Embed
+
+import os
+
+
+class Module(ModuleBase):
+
+    short_doc = 'Get information about bot.'
+
+    name = 'info'
+    aliases = (name, 'information')
+
+    async def on_call(self, msg, *args, **flags):
+        git_url = None
+        git_commit = None
+
+        if os.path.isdir('.git'):
+            program = ' && '.join((
+                'git config --get remote.origin.url',
+                'git show -s HEAD --format="latest commit made %cr by %an: \`\`\`\n%s\n\`\`\`[commit %h]({url}/commit/%H)"'
+            ))
+            process, pid = await create_subprocess_shell(program)
+            stdout, stderr = await execute_process(process, program)
+            git_url, _, git_commit = stdout.decode().strip().partition('\n')
+
+            if git_url.endswith(".git"):
+                git_url = git_url[:-4]
+            if git_url.startswith('ssh://'):
+                git_url = git_url[6:]
+            if git_url.startswith("git@"):
+                domain, _, resource = git_url[4:].partition(':')
+                git_url = f'https://{domain}/{resource}'
+            if git_url.endswith('/'):
+                git_url = git_url[:-1]
+            git_commit = git_commit.format(url=git_url)
+            repo_name = git_url.split("/")[-1]
+        else:
+            git_url = 'https://github.com/Fogapod/BotMyBot'
+            git_commit = 'Could not get information.'
+            repo_name = 'NameBotName'
+
+        try:
+            user = await self.bot.get_user_info(AUTHOR_ID)
+        except Exception:
+            author = f'Not found! His id was {AUTHOR_ID}'
+        else:
+            author = f'{user.name}#{user.discriminator}'
+
+        prefix = await get_local_prefix(msg, self.bot)
+
+        embed = Embed(
+            colour=Colour.gold(), title=repo_name,
+            url=git_url,
+            description=(
+                f'Hello, my name is **{repo_name}**\n'
+                f'I\'m a discord bot. Here is some useful info about me:\n'
+                f'My local prefix is **{prefix}**\n'
+                f'I was created by **{author}**\n'
+                f'I\'m still in very early stage of the development, feel free to send me a pr!'
+            )
+        )
+        embed.set_thumbnail(url=self.bot.user.avatar_url)
+        embed.add_field(name='git status', value=git_commit, inline=False)
+
+        await self.send(msg, embed=embed)
--- modules/module_invite.py
+++ modules/module_invite.py
@@ -1,5 +1,9 @@
 from modules.modulebase import ModuleBase
 
+from discord import Colour, Embed
+
+
+DISCORD_AUTH_URL = 'https://discordapp.com/oauth2/authorize?client_id={id}&scope=bot&permissions={perms}'
 
 class Module(ModuleBase):
     
@@ -9,4 +13,18 @@ class Module(ModuleBase):
     aliases = (name, )
 
     async def on_call(self, msg, *args, **options):
-        return 'Invite me to your server by this link: <https://discordapp.com/oauth2/authorize?client_id=394793577160376320&scope=bot&permissions=8>\nContact developar: https://discord.gg/TNXn8R7'
+        e = Embed(
+            title='TitleMyTitle', colour=Colour.gold(),
+            description='Here aren some useful links for you'
+        )
+        e.set_thumbnail(url=self.bot.user.avatar_url)
+        e.add_field(
+            name='Invite me to your guild',
+            value='   |   '.join((
+                '[admin](' + DISCORD_AUTH_URL.format(id=self.bot.user.id, perms=8) + ')',
+                '[no admin](' + DISCORD_AUTH_URL.format(id=self.bot.user.id, perms=2146958583) + ')'
+            )), inline=False
+        )
+        e.add_field(name='Contact developar', value='[development server](https://discord.gg/TNXn8R7)')
+
+        await self.send(msg, embed=e)
