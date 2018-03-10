3d957db02b15515c94719788f49ec25278ffefef
Fogapod <gish.ee18@gmail.com>
2018-03-11 01:31:54 +0300

    added module user2

---
+++ modules/module_user2.py
@@ -0,0 +1,31 @@
+from modules.modulebase import ModuleBase
+
+from utils.helpers import find_user, get_string_after_entry
+
+from discord import Embed
+
+
+class Module(ModuleBase):
+
+    usage_doc = '{prefix}{aliases} <user>'
+    short_doc = 'Get matched users list.'
+
+    name = 'user2'
+    aliases = (name, )
+    hidden = True
+
+    async def on_call(self, msg, *args, **flags):
+        user = None
+
+        if len(args) == 1:
+            users = (msg.author, )
+        else:
+            users = await find_user(
+                get_string_after_entry(args[0], msg.content),
+                msg, self.bot, max_count=20
+            )
+
+        if not users:
+            return '{warning} Users not found'
+
+        return 'Matched users:```\n' + '\n'.join(f'{i + 1}) {u.name}#{u.discriminator} {u.id}' for i, u in enumerate(users)) + '```'
