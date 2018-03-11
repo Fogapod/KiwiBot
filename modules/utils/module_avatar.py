839d59aa2338c04f23e15c7887545e6f2f308710
Fogapod <gish.ee18@gmail.com>
2018-03-11 23:44:23 +0300

    added avatar module

---
+++ modules/utils/module_avatar.py
@@ -0,0 +1,39 @@
+from modules.modulebase import ModuleBase
+
+from utils.helpers import find_user
+
+from permissions import PermissionEmbedLinks
+
+from discord import Embed
+
+
+class Module(ModuleBase):
+
+    usage_doc = '{prefix}{aliases} [user]'
+    short_doc = 'Get user avatar.'
+
+    name = 'avatar'
+    aliases = (name, 'pfp')
+    required_perms = (PermissionEmbedLinks, )
+
+    async def on_call(self, msg, *args, **flags):
+        format = flags.pop('format', 'webp')
+
+        if len(args) == 1:
+            user = msg.author
+        else:
+            user = await find_user(
+                msg.content.partition(args[0])[2].lstrip(),
+                msg, self.bot
+            )
+
+        if user is None:
+            return '{warning} User not found'
+
+        avatar_url = user.avatar_url_as(static_format=format)
+
+        e = Embed(title='direct link', url=avatar_url)
+        e.set_image(url=avatar_url)
+        e.set_footer(text=user, icon_url=avatar_url)
+
+        await self.send(msg, embed=e)
