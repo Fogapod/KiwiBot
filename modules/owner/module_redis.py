57213aa2e260b097dca405415ed2b78477154f2f
Fogapod <gish.ee18@gmail.com>
2018-04-01 13:10:39 +0300

    added redis module

---
+++ modules/owner/module_redis.py
@@ -0,0 +1,29 @@
+from objects.modulebase import ModuleBase
+from objects.permissions import PermissionBotOwner
+
+import aioredis
+
+
+class Module(ModuleBase):
+
+    usage_doc = '{prefix}{aliases} <args*>'
+    short_doc = 'Redis db access.'
+
+    name = 'redis'
+    aliases = (name, 'r')
+    required_args = 1
+    require_perms = (PermissionBotOwner, )
+    hidden = True
+
+    async def on_call(self, msg, args, **flags):
+        value = await self.bot.redis.execute(*args._args[1:])
+
+        return 'Return value: ' + self.to_string(value)
+    
+    def to_string(self, value):
+        if type(value) is list:
+            return str([self.to_string(v) for v in value])
+        elif type(value) is bytes:
+            return value.decode()
+        else:
+            return str(value)
