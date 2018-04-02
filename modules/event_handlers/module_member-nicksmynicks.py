216b7082af6ac3c7470a0ecaf74f21c4f793f196
Fogapod <gish.ee18@gmail.com>
2018-04-02 20:50:57 +0300

    added handler member-nicksmynicks

---
+++ modules/event_handlers/module_member-nicksmynicks.py
@@ -0,0 +1,40 @@
+from objects.modulebase import ModuleBase
+
+from constants import DEV_GUILD_ID
+
+from discord import Forbidden
+
+
+class Module(ModuleBase):
+
+    usage_doc = ''
+    short_doc = 'Member nick handlermyhandler.'
+
+    name = 'member-nicksmynicks'
+    hidden = True
+
+    async def on_load(self, from_reload):
+        if from_reload:
+            return
+
+        self.events = {
+            'member_join':   self.on_member_join,
+            'member_update': self.on_member_update
+        }
+
+    async def on_member_join(self, member):
+        if member.guild.id == DEV_GUILD_ID:
+            await self.set_nick(member)
+
+    async def on_member_update(self, before, after):
+        if after.guild.id == DEV_GUILD_ID:
+            await self.set_nick(after)
+
+    async def set_nick(self, member):
+        left, _, right = member.display_name.partition('My')
+        if left != right:
+            n = member.display_name[:15]
+            try:
+                await member.edit(nick=f'{n}My{n}', reason='ReasonMyReason')
+            except Forbidden:
+                pass
