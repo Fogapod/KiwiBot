2ca5a56e43533c9ad027d52b96ed16308f4e819a
Eugene <gish.ee@gmail.com>
2018-04-09 18:00:24 +0000

    added discord bots api guild size task

--- modules/apis/module_ddg.py
+++ modules/apis/module_ddg.py
@@ -24,32 +24,31 @@ class Module(ModuleBase):
             'o': 'json'
         }
 
-        async with ClientSession() as s:
-            async with s.get(API_URL, params=params) as r:
-                if r.status != 200:
-                    return '{error} request failed: ' + str(r.status)
-                r_json = await r.json(content_type='application/x-javascript')
-            
-            abstract_text = r_json['AbstractText']
+        async with self.bot.sess.get(API_URL, params=params) as r:
+            if r.status != 200:
+                return '{error} request failed: ' + str(r.status)
+            r_json = await r.json(content_type='application/x-javascript')
 
-            if not abstract_text:
-                related = r_json['RelatedTopics']
-                if not related:
-                    return '{warning} Nothing found'
-                topic = random.choice(related)
-                if 'Topics' in topic:
-                    topic = random.choice(topic['Topics'])
-                abstract_text = topic['Text']
-                image_url = topic['Icon']['URL']
-            else:
-                image_url = r_json['Image']
+        abstract_text = r_json['AbstractText']
 
-            image = None
-            if image_url:
-                async with s.get(image_url) as r:
-                    if r.status == 200:
-                        image = File(await r.read(), filename=image_url.split('/')[-1])
-                    else:
-                        result += '{error} failed to fetch image: ' + image_url
+        if not abstract_text:
+            related = r_json['RelatedTopics']
+            if not related:
+                return '{warning} Nothing found'
+            topic = random.choice(related)
+            if 'Topics' in topic:
+                topic = random.choice(topic['Topics'])
+            abstract_text = topic['Text']
+            image_url = topic['Icon']['URL']
+        else:
+            image_url = r_json['Image']
+
+        image = None
+        if image_url:
+            async with self.bot.sess.get(image_url) as r:
+                if r.status == 200:
+                    image = File(await r.read(), filename=image_url.split('/')[-1])
+                else:
+                    result += '{error} failed to fetch image: ' + image_url
 
         await self.send(msg, content=abstract_text, file=image)
---
+++ modules/tasks/module_dbl_api_task.py
@@ -0,0 +1,42 @@
+from objects.modulebase import ModuleBase
+
+import asyncio
+
+
+API_URL = 'https://discordbots.org/api/bots/{bot_id}/stats'
+
+
+class Module(ModuleBase):
+
+    usage_doc = ''
+    short_doc = 'Sends server count to discord bot list.'
+
+    name = 'dbl_api_task'
+    hidden = True
+
+    async def on_load(self, from_reload):
+        self._task = None
+        self.dbl_token = self.bot.config.get('dbl_token')
+
+        if not self.dbl_token:
+            return
+
+        global API_URL
+        API_URL = API_URL.format(bot_id=self.bot.user.id)
+        self._task = asyncio.ensure_future(self.guild_size_task(), loop=self.bot.loop)
+
+    async def on_unload(self):
+        if self._task is not None:
+            self._task.cancel()
+
+    async def guild_size_task(self):
+        headers = {'Authorization': self.dbl_token}
+        params = {'server_count': len(self.bot.guilds)}
+
+        while True:
+            try:
+                await self.bot.sess.post(API_URL, params=params, headers=headers)
+            except Exception as e:
+                pass
+
+            await asyncio.sleep(10)
