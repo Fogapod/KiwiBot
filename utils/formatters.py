9f81f854318c804d68cffbeffc734e6a20259c9f
Fogapod <gish.ee18@gmail.com>
2018-04-26 12:58:23 +0300

    revert of deleting format_response function usage, temporary removed mist of the keys

--- utils/formatters.py
+++ utils/formatters.py
@@ -5,24 +5,24 @@ async def format_response(response, message, bot):
         format_dict['warning'] = '⚠'
     if '{error}' in response:
         format_dict['error'] = '❗'
-    if '{guild}' in response and message.guild:
-        format_dict['guild'] = message.guild.name
-    if '{guild_id}' in response and message.guild:
-        format_dict['guild_id'] = message.guild.id
-    if '{channel}' in response:
-        format_dict['channel'] = getattr(message.channel, 'name', 'DMchannel')
-    if '{channel_id}' in response:
-        format_dict['channel_id'] = message.channel.id
-    if '{id}' in response: 
-        format_dict['id'] = message.author.id
-    if '{name}' in response:
-        format_dict['name'] = message.author.name
-    if '{nick}' in response:
-        format_dict['nick'] = message.author.display_name
-    if '{discrim}' in response:
-        format_dict['discrim'] = message.author.discriminator
-    if '{mention}' in response:
-        format_dict['mention'] = message.author.mention
+    # if '{guild}' in response and message.guild:
+    #     format_dict['guild'] = message.guild.name
+    # if '{guild_id}' in response and message.guild:
+    #     format_dict['guild_id'] = message.guild.id
+    # if '{channel}' in response:
+    #     format_dict['channel'] = getattr(message.channel, 'name', 'DMchannel')
+    # if '{channel_id}' in response:
+    #     format_dict['channel_id'] = message.channel.id
+    # if '{id}' in response:
+    #     format_dict['id'] = message.author.id
+    # if '{name}' in response:
+    #     format_dict['name'] = message.author.name
+    # if '{nick}' in response:
+    #     format_dict['nick'] = message.author.display_name
+    # if '{discrim}' in response:
+    #     format_dict['discrim'] = message.author.discriminator
+    # if '{mention}' in response:
+    #     format_dict['mention'] = message.author.mention
 
     return lazy_format(response, **format_dict)
 
