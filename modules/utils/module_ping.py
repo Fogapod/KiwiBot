7208991219a5e89cae2c7ec7877abd11fcef81f9
Fogapod <gish.ee18@gmail.com>
2018-05-11 09:26:57 +0300

    fixed possible domain encoding exception

--- modules/utils/module_ping.py
+++ modules/utils/module_ping.py
@@ -23,13 +23,18 @@ class Module(ModuleBase):
                 domain = domain[1:-1]
             if re.fullmatch('https?://.+', domain):
                 domain = re.sub('^https?://|/$', '', domain)
-            program = ['ping', '-c', '4', domain.encode('idna')]
-            process, pid = await create_subprocess_exec(*program)
-            stdout, stderr = await execute_process(process, program)
+            try:
+                domain = domain.encode('idna')
+            except UnicodeError:
+                pass
+            else:
+                program = ['ping', '-c', '4', domain]
+                process, pid = await create_subprocess_exec(*program)
+                stdout, stderr = await execute_process(process, program)
 
-            if process.returncode in (0, 1):  # (successful ping, 100% package loss)
-                return await self.bot.edit_message(
-                    ping_msg, f'```\n{stdout.decode() or stderr.decode()}```')
+                if process.returncode in (0, 1):  # (successful ping, 100% package loss)
+                    return await self.bot.edit_message(
+                        ping_msg, f'```\n{stdout.decode() or stderr.decode()}```')
 
         msg_timestamp = msg.edited_at or msg.created_at
         delta = round((ping_msg.created_at.timestamp() - msg_timestamp.timestamp()) * 1000)
