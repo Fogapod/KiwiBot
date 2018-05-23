938810015158d269ebf7813de64e881eacf332f7
Fogapod <gish.ee18@gmail.com>
2018-05-24 00:48:58 +0300

    added missing \n

--- objects/modulebase.py
+++ objects/modulebase.py
@@ -146,7 +146,7 @@ class ModuleBase:
             + self.name + '**: **' + e.__class__.__name__ + '**\n'
             + 'Please, report this to bot owner directly'
             + (f' or join support guild: {DEV_GUILD_INVITE}' if not ctx.guild or ctx.guild.id != DEV_GUILD_INVITE else '')
-            + '```\n' + '\n'.join(tb_text.split('\n')[-4:]) + '\n```'
+            + '\n```\n' + '\n'.join(tb_text.split('\n')[-4:]) + '\n```'
         )
 
     async def on_unload(self):
