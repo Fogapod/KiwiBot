68d03381609fc4f3b79217e5112840a03672380a
Fogapod <gish.ee18@gmail.com>
2018-06-13 22:41:21 +0300

    small fixes in impersonate command

--- modules/fun/module_impersonate.py
+++ modules/fun/module_impersonate.py
@@ -46,16 +46,24 @@ class Module(ModuleBase):
         if not channel.permissions_for(ctx.author).send_messages:
             return '{warning} You don\'t have permission to send messages to this channel'
 
+        if len(user.display_name) < 2:
+            # minimum webhook name len is 2 characters, so transparent character is required
+            nickname = f' ุ{user.display_name}'
+        else:
+            nickname = user.display_name
+
         try:
-            webhook = await channel.create_webhook(name=user.display_name)
+            webhook = await channel.create_webhook(name=nickname)
         except Exception:
-            return '{error} Failed to create webhook'
+            return '{error} Failed to create webhook. Probably maximum webhook count reached'
 
         try:
             # https://github.com/Rapptz/discord.py/issues/1242
             #
             # m = await self.bot.send_message(
-            #    args[2:], avatar_url=user.avatar_url_as(format='png'), wait=True)
+            #    args[2:], avatar_url=user.avatar_url_as(format='png'),
+            #    wait=True, response_to=ctx.message
+            # )
             #
             await self.bot.send_message(
                 webhook, args[2:], avatar_url=user.avatar_url_as(format='png'))
