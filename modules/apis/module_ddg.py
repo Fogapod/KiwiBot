cd56ddeeb0ab29b0a6aa87914f4e68c7db849d05
Kiwi <gish.ee18@gmail.com>
2018-09-14 19:27:35 +0000

    Add error handler in case ddg will decide to respond with html page instead of json...

--- modules/apis/module_ddg.py
+++ modules/apis/module_ddg.py
@@ -4,6 +4,7 @@ from objects.paginators import Paginator
 
 from discord import Embed, Colour
 
+import aiohttp
 
 API_URL = 'https://api.duckduckgo.com'
 
@@ -28,7 +29,10 @@ class Module(ModuleBase):
         async with self.bot.sess.get(API_URL, params=params) as r:
             if r.status != 200:
                 return '{error} request failed: ' + str(r.status)
-            r_json = await r.json(content_type='application/x-javascript')
+            try:
+                r_json = await r.json(content_type='application/x-javascript')
+            except aiohttp.ContentTypeError:  # (text/html; charset=utf-8) with query "osu!", ???
+                return '{error} Failed to read response'
 
         def make_embed(page):
             e = Embed(colour=Colour.gold(), title='DuckDuckGo')
