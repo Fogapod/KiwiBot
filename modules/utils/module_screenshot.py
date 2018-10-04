372638939d6b172ba0688a67ded15b0f698b0ac8
Kiwi <gish.ee18@gmail.com>
2018-10-04 08:31:17 +0000

    add sreenshot module

---
+++ modules/utils/module_screenshot.py
@@ -0,0 +1,93 @@
+from objects.modulebase import ModuleBase
+from objects.permissions import PermissionEmbedLinks, PermissionAttachFiles
+
+import time
+import asyncio
+
+from os import devnull
+
+from discord import Embed, Colour, File
+from arsenic import start_session, stop_session, services, browsers, get_session
+from arsenic.errors import UnknownArsenicError
+
+import logging
+import structlog
+
+
+logger = logging.getLogger('arsenic')
+logger.setLevel(logging.CRITICAL)
+
+structlog.configure(logger_factory=lambda: logger)
+
+
+class Module(ModuleBase):
+
+    usage_doc = '{prefix}{aliases} <url>'
+    short_doc = 'Screenshot webpage'
+
+    name = 'screenshot'
+    aliases = (name, 'ss')
+    category = 'Actions'
+    min_args = 1
+    max_args = 1
+    bot_perms = (PermissionEmbedLinks(), PermissionAttachFiles())
+
+    async def on_load(self, from_reload):
+        self.lock = asyncio.Lock()
+
+    async def on_call(self, ctx, args, **flags):
+        m = await ctx.send('Taking screenshot...')
+
+        url = args[1]
+        if url.startswith('<') and url.endswith('>'):
+            url = url[1:-1]
+        if not url.startswith(('http://', 'https://')):
+            url = 'http://' + url
+
+        try:
+            async with self.bot.sess.head(url, timeout=15) as r:
+                if (r.content_length or 0) > 100000000:
+                    return await self.bot.edit_message(
+                        m, 'Rejected to navigate')
+        except Exception:
+            return await self.bot.edit_message(m, 'Connection timeout')
+
+        await self.lock.acquire()
+ 
+        try:
+            service = service = services.Chromedriver(log_file=devnull)
+            browser = browsers.Chrome(chromeOptions={'args': ['--headless', '--disable-gpu']})
+
+            async with get_session(service, browser) as session:
+                await session.set_window_size(1920, 1080)
+                await session.get(url)
+                opened_url = await session.get_url()
+                await asyncio.sleep(2)
+                screenshot = await session.get_screenshot()
+        except UnknownArsenicError:
+            await self.bot.edit_message(
+                m, 'Unknown exception happened')
+            return
+        except Exception:
+            return await self.bot.edit_message(
+                m, 'Could not open page, please check url and try again')
+        finally:
+            try:
+               self.lock.release()
+            except Exception:
+                pass
+
+        try:
+            title = opened_url.split('/')[2]
+        except IndexError:
+            title = "Screenshot"
+
+        e = Embed(title=title[:256], colour=Colour.gold(), url=url)
+        e.set_image(url='attachment://screenshot.png')
+
+        f = File(screenshot, filename='screenshot.png')
+        e.set_footer(
+            text=f'Took {round(time.time() - (m.created_at or m.edited_at).timestamp(), 1)} seconds')
+
+        await self.bot.delete_message(m)
+        await ctx.send(embed=e, file=f)
