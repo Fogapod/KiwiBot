2b89defdeb7c2860f97af1890799059c737794c8
Kiwi <gish.ee18@gmail.com>
2018-10-14 21:20:46 +0000

    add ability to ping proxies in ping command

--- modules/utils/module_ping.py
+++ modules/utils/module_ping.py
@@ -3,7 +3,12 @@ from objects.modulebase import ModuleBase
 from utils.funcs import create_subprocess_exec, execute_process
 
 import re
+import asyncio
+import time
 
+
+PROXY_TEST_URL = 'https://httpbin.org'
+PROXY_TEST_TIMEOUT = 5
 
 class Module(ModuleBase):
 
@@ -18,7 +23,47 @@ class Module(ModuleBase):
         ping_msg = await ctx.send('Pinging ...')
 
         if len(args) == 2:
-            domain = args[1]
+            domain = args[1].lower()
+            if domain == 'proxies':
+                result = ''
+                if not self.bot.proxies:
+                    return 'I don\'t have any proxies to ping'
+
+                tasks = []
+                result = ''
+                async def ping_task(url, name):
+                    nonlocal result
+                    begin = time.time()
+                    try:
+                        r = await self.bot.sess.head(
+                                PROXY_TEST_URL, proxy=url, timeout=PROXY_TEST_TIMEOUT)
+                    except Exception as e:
+                        response_time = '-'
+                        comment = f'Ping {PROXY_TEST_URL}: {e.__class__.__name__}'
+                    else:
+                        response_time = str(round((time.time() - begin) * 1000))
+                        if len(response_time) > 4:
+                            response_time = "999+"
+
+                        response_time += 'ms'
+
+                        if r.status == 200:
+                            comment = 'ok'
+                        else:
+                            comment = f'Error pinging {PROXY_TEST_URL}: {r.status}'
+
+                    result += f'{name:<20} | {response_time:<6} | {comment}\n'
+
+                for url, name in self.bot.proxies.items():
+                    tasks.append(ping_task(url, name))
+
+                await asyncio.gather(*tasks)
+
+                header = f'{"NAME":<20} | {"TIME":<6} | COMMENT\n'
+                header += f'{"-" * 21}+{"-" * 8}+{"-" * 21}\n'
+
+                return await self.bot.edit_message(ping_msg, f'```\n{header}{result}```')
+
             if domain.startswith('<') and domain.endswith('>'):
                 domain = domain[1:-1]
             if re.fullmatch('https?://.+', domain):
