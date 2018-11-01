dbc71b540918136c4d99187f15715dde7a637094
Eugene <gish.ee18@gmail.com>
2018-11-01 16:30:00 +0300

    place author avatar on Batmen's face in slap command

--- modules/images/module_slap.py
+++ modules/images/module_slap.py
@@ -5,6 +5,7 @@ import io
 import discord
 
 from PIL import Image
+from PIL.ImageOps import mirror
 
 from utils.funcs import find_image
 
@@ -13,10 +14,20 @@ class Module(ModuleBase):
 
     usage_doc = '{prefix}{aliases} [image]'
     short_doc = 'Makes a slap meme'
+    long_doc = (
+        'Flags:\n'
+        '\t[--avatar|-a]: don\'t place author avatar on Batman\'s face if added'
+    )
 
     name = 'slap'
     aliases = (name, )
     category = 'Images'
+    flags = {
+        'avatar': {
+            'alias': 'a',
+            'bool': True
+        }
+    }
     ratelimit = (1, 3)
 
     async def on_call(self, ctx, args, **flags):
@@ -25,12 +36,29 @@ class Module(ModuleBase):
             return await ctx.warn('Give me an image to slap')
 
         source = Image.open(io.BytesIO(image))
-        s_w, s_h = source.size
-        if s_w + s_h > 10000:
+        if sum(source.size) > 10000:
             return await ctx.error('Input image is too big')
 
+        if flags.get('avatar', False):
+            author_source = None
+        else:
+            try:
+                async with self.bot.sess.get(
+                        ctx.author.avatar_url_as(format='png'), raise_for_status=True) as r:
+                    author_source = Image.open(io.BytesIO(await r.read()))
+            except Exception:
+                return await ctx.error(
+                    'Failed to download author avatar, use **--avatar** flag to disable it')
+
         template = Image.open('templates/slap')
-        t_w, t_h = template.size
+
+        if author_source is not None:
+            author_resized = author_source.resize((240, 240), Image.ANTIALIAS)
+            author_flipped = mirror(author_resized)
+            author_channels = author_flipped.split()
+            author_mask = author_channels[3] if len(author_channels) >= 4 else None
+
+            template.paste(author_flipped, (480, 200), mask=author_mask)
 
         resized = source.resize((280, 280), Image.ANTIALIAS)
         channels = resized.split()
