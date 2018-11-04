c854facc77785bb3bbd5413d53a32d57f7f96851
Eugene <gish.ee18@gmail.com>
2018-11-04 22:11:48 +0300

    add amount argument to undo command

--- modules/utils/module_undo.py
+++ modules/utils/module_undo.py
@@ -3,35 +3,53 @@ from objects.modulebase import ModuleBase
 
 class Module(ModuleBase):
 
+    usage_doc = '{prefix}{aliases} [amount]'
     short_doc = 'Removes previous command and responses'
 
     name = 'undo'
     aliases = (name, )
     category = 'Actions'
+    max_args = 1
 
     async def on_call(self, ctx, args, **flags):
-        async for msg in ctx.channel.history(
-                limit=200, before=ctx.message.edited_at or ctx.message.created_at):
+        response = ''
 
-            if msg.author != ctx.author or msg.id == ctx.message.id:
+        if len(args) == 2:
+            if not args[1].isdigit():
+                return await ctx.warn(
+                    'Amount must be a positive number',
+                    register=False, delete_after=4
+                )
+            amount = int(args[1])
+        else:
+            amount = 1
+
+        await self.bot.delete_message(ctx.message)
+
+        deleted = 0
+        failed = 0
+
+        async for msg in ctx.channel.history(
+                limit=100, before=ctx.message.edited_at or ctx.message.created_at):
+
+            if amount == 0:
+                break
+
+            if msg.author != ctx.author:
                 continue
 
             if len(await self.bot.redis.lrange(f'tracked_message:{msg.id}', 1, -1)):
                 try:
                     await self.bot.delete_message(msg, raise_on_errors=True)
                 except Exception:  # do cleanup manually
+                    failed += 1
                     await self.bot.clear_responses_to_message(msg.id)
 
-                await self.bot.delete_message(ctx.message)
+                deleted += 1
+                amount -= 1
 
-                return await ctx.send(
-                    'Deleted last command and corresponding responses',
-                    register=False, delete_after=5
-                )
-
-        await self.bot.delete_message(ctx.message)
-
-        await ctx.send(
-            'No commands found in the last 200 messages. Try deleting them manually',
-            register=False, delete_after=5
+        await self.bot.send_message(
+            ctx.channel,
+            f'Deleted responses to **{deleted}** command(s), failed to delete **{failed}** messages',
+            delete_after=4,
         )
