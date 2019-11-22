fc4ab716c6640a36468ed4c6e8a3cd58d4b6c699
Eugene <gish.ee18@gmail.com>
2019-11-22 16:19:29 +0000

    Add example subcommand, use examples as default program

--- modules/apis/module_run.py
+++ modules/apis/module_run.py
@@ -6,11 +6,12 @@ API_URL = 'https://run.iomirea.ml/api/v0/languages'
 
 class Module(ModuleBase):
 
-    usage_doc = '{prefix}{aliases} <language> <program>'
+    usage_doc = '{prefix}{aliases} <language> [program]'
     short_doc = 'Execute code using IOMirea run service'
     long_doc = (
         'Subcommands:\n'
-        '\tlist: show list of languages'
+        '\tlist: show list of languages\n'
+        '\texample <language>: shows example program'
     )
 
     name = 'run'
@@ -29,41 +30,67 @@ class Module(ModuleBase):
 
         for lang in data:
             for alias in lang["aliases"]:
-                self.langs[alias] = lang["name"]
+                self.langs[alias] = lang
+
+    def _get_language(self, alias):
+        alias = alias.lower()
+
+        # inefficient
+        for l in self.langs.values():
+            if alias == l["name"]:
+                return l
+
+        return self.langs.get(alias)
 
     async def on_call(self, ctx, args, **options):
-        result = ''
+        async with ctx.channel.typing():
+            return await self._on_call(ctx, args, **options)
 
+    async def _on_call(self, ctx, args, **options):
         if args[1].lower() == 'list':
             last_name = ""
-            for k, v in sorted(self.langs.items(), key=lambda x: x[1]):
+            for k, v in sorted(self.langs.items(), key=lambda x: x[1]["name"]):
                 if v == last_name:
                     result += ', ' + k
                 else:
-                    result += f'\n`{v}`: {k}'
+                    result += f'\n`{v["name"]}`: {k}'
                     last_name = v
 
             return result
 
+        if len(args) < 2:
+            return await ctx.warn("Not enough arguments passed")
+
+        if args[1].lower() == "example":
+            language = self._get_language(args[2])
+            if not language:
+                return await ctx.warn("Unknown language, try checking list of supported languages")
+
+            nl = '\n'
+            return f'```{language["name"]}{nl}{language["example"]}```'
+
+        result = ''
+
         cleaned, from_codeblock = cleanup_code(args[1:])
         if from_codeblock is None:
-            lower_input = args[1].lower()
+            input_lang = args[1]
 
             cleaned, _ = cleanup_code(args[2:])
             # we don't care about parsed language because
             # it's already defined explicitly
         else:
-            lower_input = from_codeblock.lower()
+            input_lang = from_codeblock
 
-        if lower_input in self.langs.values():
-            language = lower_input
-        else:
-            language = self.langs.get(lower_input)
+        language = self._get_language(input_lang)
 
         if not language:
             return await ctx.warn('Invalid language, try checking list of supported languages')
 
-        async with self.bot.sess.post(f"{API_URL}/{language}", params=dict(merge="1"), json=dict(code=cleaned)) as r:
+        payload = {}
+        if cleaned:
+            payload['code'] = cleaned
+
+        async with self.bot.sess.post(f"{API_URL}/{language['name']}", params=dict(merge="1"), json=payload) as r:
             if r.status != 200:
                 message = (await r.json())["message"]
                 return await ctx.error(f'Error connecting to IOMirea API. Please, try again later: {message}')
